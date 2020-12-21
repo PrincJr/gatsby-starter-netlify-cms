2bb2b99905493219294277f8d69d109ff1a814b7
PrincJr <juniorsilvaasafe@gmail.com>
2020-12-20 21:26:03 -0400

    atualizacao meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Blog do Bendev',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Bem vindo(a) ao Blog do bendev junior, aqui falo sobre mundo da tecnologia novidades e afins.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
